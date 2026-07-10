6d41e6875ad856eaa834aa90323041a16b517ca2
Magda <magdaverocas@gmail.com>
2026-07-09 20:08:47 -0600

    Modificaciones en ruta detalle receta

--- server/routes/detalleRecetaRoutes.js
+++ server/routes/detalleRecetaRoutes.js
@@ -11,16 +11,42 @@ router.get("/:id", async (req, res) => {
 
         const resultado = await pool.query(
             `
-            SELECT
-                id_detalle_receta,
-                id_receta,
-                id_ma,
-                id_producto_insumo,
-                cantidad_utilizada,
-                descripcion
-            FROM detalle_receta
-            WHERE id_receta = $1
-            ORDER BY id_detalle_receta
+             SELECT
+                r.id_receta,
+                r.nombre_receta,
+                r.cantidad_producida_base,
+                r.imagen_url,
+
+                dr.id_detalle_receta,
+                dr.cantidad_utilizada,
+                dr.descripcion,
+
+                COALESCE(mp.nombre, p.nombre) AS nombre_insumo,
+
+                CASE
+                    WHEN mp.id_ma IS NOT NULL THEN mp.unidad_medida
+                    ELSE 'unidad(es)'
+                END AS unidad_medida,
+
+                CASE
+                    WHEN mp.id_ma IS NOT NULL THEN 'Materia prima'
+                    ELSE 'Producto elaborado'
+                END AS tipo_insumo
+
+            FROM receta r
+
+            INNER JOIN detalle_receta dr
+                ON r.id_receta = dr.id_receta
+
+            LEFT JOIN materia_prima_y_cd mp
+                ON dr.id_ma = mp.id_ma
+
+            LEFT JOIN producto p
+                ON dr.id_producto_insumo = p.id_producto
+
+            WHERE r.id_receta = $1
+
+            ORDER BY dr.id_detalle_receta
             `,
             [id]
         );
