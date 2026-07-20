ecee63d9f83c6fed5b18519cd2ac73446f1a3206
Magda <magdaverocas@gmail.com>
2026-07-20 15:47:14 -0600

    si

--- client/public/JavaScript/productos.js
+++ client/public/JavaScript/productos.js
@@ -1,23 +1,10 @@
 async function cargarProductos() {
     const respuesta = await fetch("/api/productos");
     const productos = await respuesta.json();
-
-    const tabla = document.getElementById("tablaProductos");
-
-    productos.forEach(producto => {
-        tabla.innerHTML += `
-            <tr>
-                <td>${producto.id_producto}</td>
-                <td>${producto.nombre}</td>
-                <td>${producto.tipo}</td>
-                <td>${producto.precio_venta}</td>
-                <td>${producto.margen_gananciab_esperado}</td>
-                <td>${producto.estado}</td>
-            </tr>
-        `;
+    
+     $('#tabla').bootstrapTable({
+        data: productos
     });
-
-    $('#tabla').bootstrapTable();
 }
 
 cargarProductos();
