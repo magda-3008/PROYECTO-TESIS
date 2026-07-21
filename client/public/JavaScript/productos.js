ba205c5ebf69a60a1b72ef4312bc95aebfce8d7b
Magda <magdaverocas@gmail.com>
2026-07-20 20:20:54 -0600

    prueba

--- client/public/JavaScript/productos.js
+++ client/public/JavaScript/productos.js
@@ -27,16 +27,7 @@ async function cargarProductos() {
     new DataTable("#tabla", {
     pageLength: 10,
     lengthMenu: [5, 10, 25, 50],
-    autoWidth: false,
-    scrollX: true,
-   columnDefs: [
-    { width: "30px", targets: 0 },
-    { width: "100px", targets: 1 },
-    { width: "100px", targets: 2 },
-    { width: "80px", targets: 3 },
-    { width: "80px", targets: 4 },
-    { width: "80px", targets: 5 }
-],
+    scrollX: true,    
     language: {
         url: "https://cdn.datatables.net/plug-ins/2.3.2/i18n/es-ES.json"
     }
