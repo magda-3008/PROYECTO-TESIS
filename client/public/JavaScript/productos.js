8fefc234bf928b9e8e36032736054bb44bd9684b
Magda <magdaverocas@gmail.com>
2026-07-31 15:38:43 -0600

    Estilo de modales

--- client/public/JavaScript/productos.js
+++ client/public/JavaScript/productos.js
@@ -412,6 +412,60 @@ document.querySelectorAll("#tabsProductos .nav-link").forEach(tab=>{
 
 });
 
+document.getElementById("tipoEntrada")
+.addEventListener("change", function(){
+
+    const labelCantidad = document.querySelector(
+        'label[for="cantidadEntrada"]'
+    );
+
+    if(this.value === "PRODUCCION"){
+        labelCantidad.textContent = "Cantidad de lotes";
+    }else{
+        labelCantidad.textContent = "Cantidad ingresada";
+    }
+
+});
+
+function configurarTipoEntrada(producto){
+
+    const selectTipo = document.getElementById("tipoEntrada");
+
+    selectTipo.innerHTML = `
+        <option value="" selected disabled>
+            Seleccione una opción
+        </option>
+    `;
+
+    if(producto.tipo === "Reventa"){
+        selectTipo.innerHTML += `
+            <option value="COMPRA">
+                Compra
+            </option>
+            <option value="ENTRADA">
+                Ajuste de inventario
+            </option>
+            <option value="OTRO">
+                Otro
+            </option>
+        `;
+
+    }else if(producto.tipo === "Elaborado"){
+
+        selectTipo.innerHTML += `
+            <option value="PRODUCCION">
+                Producción
+            </option>
+            <option value="ENTRADA">
+                Ajuste de inventario
+            </option>
+            <option value="OTRO">
+                Otro
+            </option>
+        `;
+    }
+}
+
 //acciones
 function abrirModalPerdida(producto){
     productoSeleccionado = producto;
@@ -433,6 +487,10 @@ function abrirModalEntrada(producto){
     document.getElementById("nombreProductoEntrada").textContent = producto.nombre;
     document.getElementById("stockActualEntrada").textContent = producto.stock_actual;
     document.getElementById("tipoProductoEntrada").textContent = producto.tipo;
+
+
+    configurarTipoEntrada(producto);
+
 
     const modal = new bootstrap.Modal(
         document.getElementById("modalEntrada")
