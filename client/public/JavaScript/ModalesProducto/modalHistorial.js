99fd8ae121bc802e9b5413a364a9cf89cd5fc5ea
Magda <magdaverocas@gmail.com>
2026-08-01 21:25:10 -0600

    ahora si a ver si da xd

--- client/public/JavaScript/ModalesProducto/modalHistorial.js
+++ client/public/JavaScript/ModalesProducto/modalHistorial.js
@@ -2,7 +2,7 @@ async function abrirHistorial(producto){
      try {
 
         const respuesta = await fetch(
-            `/api/movimientos/${idProducto}`
+            `/api/movimientos/${producto.id_producto}`
         );
 
         const movimientos = await respuesta.json();
