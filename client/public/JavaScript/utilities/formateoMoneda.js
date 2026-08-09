640d55a5bf28714cea34d3ac29f045cc35011d6d
Magda <magdaverocas@gmail.com>
2026-08-08 19:27:47 -0600

    correcciones de formato

--- client/public/JavaScript/utilities/formateoMoneda.js
+++ client/public/JavaScript/utilities/formateoMoneda.js
@@ -1,14 +1,14 @@
 function formatoMoneda(valor) {
-	// Si es un objeto con método getValue(), se extrae el valor
 	if (valor && typeof valor.getValue === 'function') {
 		valor = valor.getValue();
 	}
+
 	const num = Number(valor);
+
 	if (isNaN(num)) return '';
-	return new Intl.NumberFormat("es-NI", {
-		style: "currency",
-		currency: "NIO",
+
+	return `C$ ${new Intl.NumberFormat("es-NI", {
 		minimumFractionDigits: 2,
 		maximumFractionDigits: 2
-	}).format(num);
+	}).format(num)}`;
 }
