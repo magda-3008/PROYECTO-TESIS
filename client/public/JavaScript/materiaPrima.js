96d88e6314d131c1d37c3eddff5edd0bf7c2b675
Magda <magdaverocas@gmail.com>
2026-09-08 10:25:55 -0600

    cambiando nombre

--- client/public/JavaScript/materiaPrima.js
+++ client/public/JavaScript/materiaPrima.js
@@ -76,7 +76,7 @@ const vistas = {
                                 <i class="bi bi-cart-plus"></i>
                             </button>
                         
-                            <button class="btnAccion btnPerdida" title="Registrar pérdida">
+                            <button class="btnAccion btnSalida" title="Registrar pérdida">
                                 <i class="bi bi-cart-dash"></i>
                             </button>
 
