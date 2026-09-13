e8a4be7cf99c00be7b8532f2db14af6fd2ec3578
Magda <magdaverocas@gmail.com>
2026-09-12 21:30:18 -0600

    me faltaba una coma XD

--- server/routes/detalleRecetaRoutes.js
+++ server/routes/detalleRecetaRoutes.js
@@ -21,7 +21,7 @@ router.get("/:id", async (req, res) => {
                 dr.id_detalle_receta,
                 dr.cantidad_utilizada,
                 dr.cantidad_ingresada,
-                dr.unidad_ingresada
+                dr.unidad_ingresada,
 
                 COALESCE(mp.nombre, p.nombre) AS nombre_insumo,
 
