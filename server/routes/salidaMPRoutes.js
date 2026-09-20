d2859fe6736aaac8b7a26a81278288f9f129e34a
Magda <magdaverocas@gmail.com>
2026-09-20 16:47:55 -0600

    corrigiendo

--- server/routes/salidaMPRoutes.js
+++ server/routes/salidaMPRoutes.js
@@ -279,57 +279,57 @@ async function registrarMovimientoSalida(
 // A UNIDAD DE MEDIDA
 // ==========================================
 
-function convertirCantidadAUnidadMedida(
-    cantidad,
-    materiaPrima
-) {
+// function convertirCantidadAUnidadMedida(
+//     cantidad,
+//     materiaPrima
+// ) {
 
-    const cantidadHumana = Number(cantidad);
+//     const cantidadHumana = Number(cantidad);
 
-    const unidadMedida =
-        String(
-            materiaPrima.unidad_medida || ""
-        ).trim();
+//     const unidadMedida =
+//         String(
+//             materiaPrima.unidad_medida || ""
+//         ).trim();
 
-    const unidadExistencia =
-        String(
-            materiaPrima.unidad_existencia || ""
-        ).trim();
+//     const unidadExistencia =
+//         String(
+//             materiaPrima.unidad_existencia || ""
+//         ).trim();
 
-    const unidadPorPaquete =
-        Number(
-            materiaPrima.unidad_por_paquete
-        );
+//     const unidadPorPaquete =
+//         Number(
+//             materiaPrima.unidad_por_paquete
+//         );
 
-    if (
-        !Number.isFinite(cantidadHumana) ||
-        cantidadHumana <= 0
-    ) {
-        throw new Error(
-            "La cantidad debe ser mayor que cero."
-        );
-    }
+//     if (
+//         !Number.isFinite(cantidadHumana) ||
+//         cantidadHumana <= 0
+//     ) {
+//         throw new Error(
+//             "La cantidad debe ser mayor que cero."
+//         );
+//     }
 
-    // Las unidades representan directamente
-    // la misma unidad de inventario.
-    if (
-        unidadMedida.toLowerCase() ===
-        unidadExistencia.toLowerCase()
-    ) {
-        return cantidadHumana;
-    }
+//     // Las unidades representan directamente
+//     // la misma unidad de inventario.
+//     if (
+//         unidadMedida.toLowerCase() ===
+//         unidadExistencia.toLowerCase()
+//     ) {
+//         return cantidadHumana;
+//     }
 
-    if (
-        !Number.isFinite(unidadPorPaquete) ||
-        unidadPorPaquete <= 0
-    ) {
-        throw new Error(
-            "La materia prima no tiene una equivalencia de presentación válida."
-        );
-    }
+//     if (
+//         !Number.isFinite(unidadPorPaquete) ||
+//         unidadPorPaquete <= 0
+//     ) {
+//         throw new Error(
+//             "La materia prima no tiene una equivalencia de presentación válida."
+//         );
+//     }
 
-    return cantidadHumana * unidadPorPaquete;
-}
+//     return cantidadHumana * unidadPorPaquete;
+// }
 
 
 module.exports = router;
